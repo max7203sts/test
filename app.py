d7a9f9173c7f982ea640d783a5bf2089794be61d
shiluru748 <138384813+shiluru748@users.noreply.github.com>
2023-07-03 12:08:39 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, abort
+
+from linebot import LineBotApi, WebhookHandler
+
+from linebot.exceptions import InvalidSignatureError
+
+from linebot.models import *
+
+#======python的函數庫==========
+import tempfile, os
+import datetime
+import time
+import json
+#======python的函數庫==========
+
+app = Flask(__name__)
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+# Channel Access Token
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+# Channel Secret
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+
+# # 監聽所有來自 /callback 的 Post Request
+# @app.route("/callback", methods=['POST'])
+# def callback():
+#     # get X-Line-Signature header value
+#     signature = request.headers['X-Line-Signature']
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
+#     # handle webhook body
+#     try:
+#         handler.handle(body, signature)
+#     except InvalidSignatureError:
+#         abort(400)
+#     return 'OK'
+
+#receive msg
+def receive_msg(request):
+    try:
+        body = request.get_data(as_text=True)
+        json_data = json.loads(body)                           # json 格式化收到的訊息
+        signature = request.headers['X-Line-Signature']
+        handler.handle(body, signature)
+        tk = json_data['events'][0]['replyToken']       # 取得 reply token
+        tp = json_data['events'][0]['message']['type']  # 取得 message 的類型
+        if tp == 'text':
+            msg = json_data['events'][0]['message']['text']   # 取得使用者發送的訊息
+            line_bot_api.reply_message(tk,TextSendMessage(text=msg))
+        if tp == 'sticker':
+            # 如果收到的訊息是表情貼圖
+            stickerId = json_data['events'][0]['message']['stickerId'] # 取得 stickerId
+            packageId = json_data['events'][0]['message']['packageId'] # 取得 packageId
+            # 使用 StickerSendMessage 方法回傳同樣的表情貼圖
+            line_bot_api.reply_message(tk,StickerSendMessage(sticker_id=stickerId, package_id=packageId))
+    except:
+        print('error', body)
+    return
+        
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
